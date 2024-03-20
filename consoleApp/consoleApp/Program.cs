9d1f0e4df45e17e747349cd1137792780c6da20d
Alina Vergunova <alinadeveloper84@gmail.com>
2024-03-20 15:18:19 +0300

    changed

--- consoleApp/consoleApp/Program.cs
+++ consoleApp/consoleApp/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace consoleApp;
+namespace consoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        Console.WriteLine("Hello World!");
     }
 }
 
